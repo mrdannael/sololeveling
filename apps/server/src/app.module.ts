dfbf0c6ea8191666a5480a40b86201c0f4ccae11
mrdannael <mr.dannael@gmail.com>
2024-03-02 01:46:53 +0100

    feat(server): add healthcheck endpoint for further development

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller.js";
 import { AppService } from "./app.service.js";
+import { HealthModule } from "./health/health.module.js";
 
 @Module({
-  imports: [],
+  imports: [HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
