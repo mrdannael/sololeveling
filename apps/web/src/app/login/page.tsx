b1081a6a83a95eaa63123f6b7a44a9223f8d13c4
mrdannael <mr.dannael@gmail.com>
2024-03-05 13:54:05 +0100

    feat(web): start adding login page

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <form action="api/login">
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
