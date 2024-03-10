fae1aa0f02b8aad04b844534fb458f855bfaabc0
mrdannael <mr.dannael@gmail.com>
2024-03-10 13:30:20 +0100

    feat(web): start adding sign up page

---
+++ apps/web/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignupPage() {
+  return (
+    <form action="api/signup">
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
