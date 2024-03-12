f5a3702b670553a1f6a8dab49cca4d52b0bb965f
mrdannael <mr.dannael@gmail.com>
2024-03-12 15:24:32 +0100

    feat(web): extend sign up form fields

--- apps/web/src/app/signup/page.tsx
+++ apps/web/src/app/signup/page.tsx
@@ -1,6 +1,8 @@
 export default function SignupPage() {
   return (
-    <form action="api/signup">
+    <form action="localhost:3001/user" method="POST">
+      <input type="text" name="name" placeholder="Name" required />
+      <input type="text" name="username" placeholder="Username" required />
       <input type="email" name="email" placeholder="Email" required />
       <input type="password" name="password" placeholder="Password" required />
       <button type="submit">Sign Up</button>
