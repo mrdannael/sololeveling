f906fa414f1c026ee9880457d02acff962ab9157
mrdannael <mr.dannael@gmail.com>
2024-03-14 12:47:15 +0100

    chore(web): remove hello world

--- apps/web/src/app/page.spec.tsx
+++ apps/web/src/app/page.spec.tsx
@@ -5,6 +5,6 @@ import Home from "./page";
 describe("Home", () => {
   it("should renders a heading", () => {
     const { getByText } = render(<Home />);
-    expect(getByText("Hello World!")).toBeVisible();
+    expect(getByText("Sololeveling")).toBeVisible();
   });
 });
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="grid place-items-center min-h-screen">
-      <h1>Hello World!</h1>
+      <h1>Sololeveling</h1>
     </main>
   );
 }
